12346cb85ebee231d0658f1a688a87ce6ca240f0
Hallan Christian <hallancma@gmail.com>
2020-01-09 02:03:36 -0300

    Atualizando projeto

--- Mod-05-Semana-01/Aula 02/Projeto-Jobify/index.js
+++ Mod-05-Semana-01/Aula 02/Projeto-Jobify/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 const sqlite = require('sqlite');
-const dbConnection = sqlite.open('banco.sqlite', { Promise });
+const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
+  Promise
+});
+
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
